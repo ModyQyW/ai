3656efe6d416fd64a2433bd6c98a186c28ae5e56
Panpan <wurui.dev@gmail.com>
2026-06-18 18:37:36 +0800

    chore: bump shell

--- bump-global-agents-md-local.sh
+++ bump-global-agents-md-local.sh
@@ -5,3 +5,5 @@ dirname=$(dirname "$0")
 bunx cpy-cli "$dirname"/AGENTS.md ~/AGENTS.md --update
 bunx cpy-cli "$dirname"/AGENTS.md ~/.codex/AGENTS.md --update
 bunx cpy-cli "$dirname"/AGENTS.md ~/.claude/CLAUDE.md --update
+bunx cpy-cli "$dirname"/AGENTS.md ~/.zcode/AGENTS.md --update
+bunx cpy-cli "$dirname"/AGENTS.md ~/.reasonix/AGENTS.md --update
